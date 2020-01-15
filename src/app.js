47d6f03acf9a3c3923383665f328abb3a217d802
Matheus Souza <matheushrt@gmail.com>
2020-01-15 17:12:56 -0300

    created and configured server

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import createError from 'http-errors';
+import express from 'express';
+import './config/database';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import routes from './routes';
+
+// load server application
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// load routes
+routes(app);
+
+// catch 404
+app.use((req, res, next) => {
+  const error = createError(404);
+  console.error(error);
+  res.status(error.status).send(error.message);
+});
+
+export default app;
